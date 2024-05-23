e9f29b25b70170e12157de621a7c0509a2d40caf
Arjunkuls <arjunkuls@gmail.com>
2024-05-23 13:59:03 +0530

    chore: Refactor code for generating text from image using GPT AI

--- geminiAI.py
+++ geminiAI.py
@@ -11,4 +11,18 @@ def generate_text_from_image_gemini(text_prompt, img_path):
     model = genai.GenerativeModel("gemini-pro-vision")
     response = model.generate_content([repr(text_prompt), img])
     response.resolve()
-    return response
+    return response.text
+
+def generate_text_from_text_gemini(text_prompt):
+    os.environ["GOOGLE_API_CREDENTIALS"] = constants.gemini_api_key
+    genai.configure(api_key=os.environ.get("GOOGLE_API_CREDENTIALS"))
+    model = genai.GenerativeModel("gemini-pro")
+    response = model.generate_content([repr(text_prompt)])
+    response.resolve()
+    return response.text
+
+
+# Example to generate text
+text_prompt = "Extract text"
+
+img_path = "imgs/shopBill.jpg"
--- gptAI.py
+++ gptAI.py
@@ -1,29 +1,82 @@
 import base64
-from openai import OpenAI
+import requests
 import constants
 
 
 
-def generate_text_from_image_gpt(text_prompt, img_path):
-    client = OpenAI(api_key=constants.openai_api_key)
-    with open(img_path, 'rb') as image_file:
-        image_data = image_file.read()
+def generate_text_from_image_gpt(text_prompt, image_path):
+    # OpenAI API Key
+    api_key = constants.openai_api_key
 
-    image_base64 = base64.b64encode(image_data).decode('utf-8')
-
-    response = client.completions.create(model='gpt-4o',
-    prompt=f"Image: {image_base64}\nText prompt: {text_prompt}",
-    max_tokens=100,
-    temperature=0.7,
-    n=1,
-    stop=None)
-
-    # Extract the generated text from the response
-    generated_text = response.choices[0].text.strip()
-
-    return generated_text
+    def encode_image(image_path):
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
 
 
+    # Getting the base64 string
+    base64_image = encode_image(image_path)
+
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+    "model": "gpt-4o",
+    "messages": [
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": text_prompt
+            },
+            {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}"
+            }
+            }
+        ]
+        }
+    ],
+    "max_tokens": 600
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    return (response.json().get("choices")[0]["message"]["content"])
+
+
+
+def generate_text_from_text_gpt(text_prompt):
+    # OpenAI API Key
+    api_key = constants.openai_api_key
+
+    headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+    "model": "gpt-4",
+    "messages": [
+        {
+        "role": "user",
+        "content": [
+            {
+            "type": "text",
+            "text": text_prompt
+            }
+        ]
+        }
+    ],
+    "max_tokens": 100
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    return (response.json().get("choices")[0]["message"]["content"])
 
 
 
